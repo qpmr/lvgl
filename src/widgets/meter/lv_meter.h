bb208b2b74e69ea36f11d5e274bca50e8d3758d4
Ilja Byckevich <iljabyckevich@gmail.com>
2023-02-28 20:34:09 +0300

    feat(meter): Add linear meter widget
    Based on the current meter expand them for supporting vertical
    and horizontal linear meter. The algorithms and api for
    the old meter is still the same for the backward compatibility.
    For using another form of meter just change the form(by default form is round):
    
     `lv_meter_set_indicator_form(obj, LV_METER_INDICATOR_FORM_LINEAR);`
    
    Also you can set the direction of scale(by default: left to rigth and up to down):
    
     `lv_obj_set_style_base_dir(meter, LV_BASE_DIR_RTL, 0);`
    
    And swap the labels with needle, if needed:
    
     `lv_meter_set_needle_side(scale, toggle_side);`
    
    If widget width >= height the result form is horizontal otherwise it is a vertical.
    All current meters can be linear, EXCEPT the clock meter, because you
    know, it's a clock :)
    
    issue #10986

--- src/widgets/meter/lv_meter.h
+++ src/widgets/meter/lv_meter.h
@@ -34,9 +34,16 @@ enum {
     LV_METER_INDICATOR_TYPE_NEEDLE_IMG,
     LV_METER_INDICATOR_TYPE_NEEDLE_LINE,
     LV_METER_INDICATOR_TYPE_SCALE_LINES,
+    LV_METER_INDICATOR_TYPE_NEEDLE_TRIANGLE,
     LV_METER_INDICATOR_TYPE_ARC,
 };
 typedef uint8_t lv_meter_indicator_type_t;
+
+enum {
+    LV_METER_INDICATOR_MODE_ROUND,
+    LV_METER_INDICATOR_MODE_LINEAR,
+};
+typedef uint8_t lv_meter_indicator_mode_t;
 
 typedef struct {
     lv_meter_indicator_type_t type;
@@ -90,8 +97,10 @@ typedef struct {
         int16_t r_mod;
         uint16_t angle_range;
         int16_t rotation;
+        uint8_t toggle_needle_side;
     } scale;
     lv_ll_t indicator_ll;
+    lv_meter_indicator_mode_t mode;
 } lv_meter_t;
 
 extern const lv_obj_class_t lv_meter_class;
@@ -117,6 +126,10 @@ typedef enum {
  * @return pointer to the created meter
  */
 lv_obj_t * lv_meter_create(lv_obj_t * parent);
+
+/*=====================
+ * Add scale
+ *====================*/
 
 /**
  * Set the properties of the ticks of a scale
@@ -231,6 +244,30 @@ void lv_meter_set_indicator_start_value(lv_obj_t * obj, lv_meter_indicator_t * i
  * @param value         the new value
  */
 void lv_meter_set_indicator_end_value(lv_obj_t * obj, lv_meter_indicator_t * indic, int32_t value);
+
+
+/*=====================
+ * Set indicator options
+ *====================*/
+
+
+/**
+ * Set indicator mode. By default for is 'LV_METER_INDICATOR_MODE_ROUND'
+ * For the classical 'LV_METER_INDICATOR_MODE_ROUND' meter
+ * height and width of the 'obj' must be the same!
+ * @param obj           pointer to a meter object
+ * @param mode          type of meter mode
+ */
+void lv_meter_set_indicator_mode(lv_obj_t * obj, lv_meter_indicator_mode_t mode);
+
+/**
+ * Swap labels with needle. Usable for vertical meter.
+ * By default needle is under the scale and labels is above the scale(horizontal meter)
+ * For the vertical meter: needle is on the right and labels is on the left
+ * @param obj         pointer to a meter object
+ * @param toggle_side If true - the labels will be swapped with the needle once.
+ */
+void lv_meter_set_needle_side(lv_obj_t * obj, bool toggle_side);
 
 /**********************
  *      MACROS
